00ff14405e921d5d778fd0bbdcb30f39dc5ab346
Farjanul Nayem <farjanul@Farjanuls-MacBook-Pro.local>
2021-07-29 01:38:19 +0600

    dot env intialize

--- LMSDemo/settings/base.py
+++ LMSDemo/settings/base.py
@@ -1,9 +1,10 @@
 import os
+from decouple import config
 
-BASE_DIR = os.path.dirname(os.path.dirname(
-    os.path.dirname(os.path.abspath(__file__))))
-SECRET_KEY = '-05sgp9!deq=q1nltm@^^2cc+v29i(tyybv3v2t77qi66czazj'
-DEBUG = True
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
 ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
--- LMSDemo/urls.py
+++ LMSDemo/urls.py
@@ -16,11 +16,13 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls import url
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path, include
 from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', lambda request: HttpResponse('Welcome!!! Django LMS rest-auth boilerplate')),
     path('user/', include('accounts.urls')),
     url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
     url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
